90574e85ab89f2d9c9bfb4a298323c8be5bca2db
livde <liv-dev@hotmail.com>
2024-10-06 22:54:23 -0400

    w jarto

---
+++ anime.js
@@ -0,0 +1,7 @@
+// anime.js
+function isPopularAnime(score) {
+    return score >= 8;  // Consideramos anime popular si su puntuación es mayor o igual a 8
+  }
+  
+  module.exports = isPopularAnime;
+  
---
+++ anime.test.js
@@ -0,0 +1,5 @@
+// anime.test.js
+test('verifica que 1 + 1 es igual a 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+  
